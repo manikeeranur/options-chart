934053134d56ea423bba10516c6b8340a4c74358
Manikandan Arumugam <manikandan.arumugam@impacteers.com>
2026-02-21 17:31:27 +0530

    live testing

---
+++ app/live/page.tsx
@@ -0,0 +1,12 @@
+import MinuteAnalysisAllInOne from "@/components/minute-comparison-table";
+
+
+const page = () => {
+  return (
+    <div>
+      <MinuteAnalysisAllInOne />
+    </div>
+  );
+};
+
+export default page;
